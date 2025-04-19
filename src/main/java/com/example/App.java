6e95abfd54ddb997452ac6d78e544977a2968a9d
Abhinav <abhinav>
2025-04-19 13:18:51 +0530

    First maven commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven + Jenkins!");
+    }
+
+    public static String greet() {
+        return "Hello, World!";
+    }
+}
