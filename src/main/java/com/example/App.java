6d0f54cc90861a02752af25fdae6d37c60b1b3c2
abhinav4567-eng <abhinavadarshsingh007@gmail.com>
2025-04-22 08:47:45 +0000

    First commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven + Jenkins!");
+        System.out.println("Hello, Maven + Jenkins!s here!");
     }
 
     public static String greet() {
